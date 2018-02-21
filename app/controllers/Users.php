c2adbf224270b1f5eb8ab7fd2d80eae1ac33c0d4
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-20 17:49:06 -0800

    Users can now log in

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -118,10 +118,29 @@
                     $data['password_err'] = 'You must enter a password';
                 }
 
+                // Check for user/email
+                if($this->userModel->findUserByEmail($data['email'])) {
+                    // User found
+                } else {
+                    // User not found
+                    $data['email_err'] = 'No user found.';
+                }
+
                 // Make sure errors are empty
                 if(empty($data['email_err']) && empty($data['password_err'])) {
                     // Validated
-                    die('SUCCESS');
+                    // Check and set logged in user
+                    $loggedInUser = $this->userModel->login($data['email'], $data['password']);
+
+                    if($loggedInUser) {
+                        // Create Session
+                        die('Success');
+                    } else {
+                        $data['password_err'] = 'Password incorrect. Try again';
+
+                        $this->view('users/login', $data);
+                    }
+
                 } else {
                     // Load view with errors
                     $this->view('users/login', $data);
