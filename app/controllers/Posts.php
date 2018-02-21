cb678ad3740f1b03030c1de4181e3e9a3a3ddba0
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-20 22:30:49 -0800

    Users can now delete their posts, nav now shows username

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -105,6 +105,7 @@
             } else {
                 // Get existing post from model
                 $post = $this->postModel->getPostById($id);
+
                 // Check for ownership
                 if($post->user_id != $_SESSION['user_id']) {
                     redirect('posts');
@@ -130,6 +131,27 @@
             ];
             
             $this->view('posts/show', $data);
+        }
+        
+        public function delete($id) {
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                // Get existing post from model
+                $post = $this->postModel->getPostById($id);
+                
+                // Check for ownership
+                if($post->user_id != $_SESSION['user_id']) {
+                    redirect('posts');
+                }
+                
+                if($this->postModel->deletePost($id)) {
+                    flash('post_message', 'Post removed');
+                    redirect('posts');
+                } else {
+                    die('Post was not deleted.');
+                }
+            } else {
+                redirect('posts');
+            }
         }
     }
 ?>
