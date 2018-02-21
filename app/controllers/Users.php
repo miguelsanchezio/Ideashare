6e000658476361188e0c935db7c1358a462f13b0
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-20 16:08:06 -0800

    Added email check to check if email is already in use

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -1,7 +1,7 @@
 <?php
     class Users extends Controller {
         public function __construct() {
-
+            $this->userModel = $this->model('User');
         }
 
         public function register() {
@@ -32,6 +32,11 @@
                 // Validate Email
                 if(empty($data['email'])) {
                     $data['email_err'] = 'You must enter an email';
+                } else {
+                    // Check email
+                    if($this->userModel->findUserByEmail($data['email'])) {
+                        $data['email_err'] = 'That email is already in use.';
+                    }
                 }
 
                 // Validate Password
